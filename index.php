c331c3193ab29a85a2deda8b31bf5ebe173f8709
roberto-palanga <r.palanga94@gmail.com>
2024-04-02 18:42:01 +0200

    stilizzazione grafica e bonus 1

--- index.php
+++ index.php
@@ -5,28 +5,35 @@ class Movie
     public $name;
     public $playbill;
     public $language;
-    public $genre;
+    public $genre = [];
     public $duration;
     public $description;
     public $director;
 
-    function __construct($_name, $_language, $_description)
+    function __construct($_name, $_language, $_description,$__playbill, $__genre)
     {
         $this->name = $_name;
         $this->language = $_language;
         $this->description = $_description;
-        // $this->playbill = $_playbill;
+        $this->playbill = $__playbill;
+        $this->genre = $__genre;
     }
 
     public function maker(){
         return $this->name;
     }
+
+    public function type(){
+        $newGenre = join(" , ", $this->genre);
+        return $newGenre;
+        
+    }
 }
 
-$movie1 = new Movie("Ritorno al futuro", "Eng", "Marty McFly viaggia indietro nel tempo, sconvolgendo accidentalmente la sua stessa esistenza. Deve riparare il passato per garantirsi il futuro.");
-$movie2 = new Movie("Harry Potter", "Ita", "Un orfano scopre di essere un mago, frequenta la scuola di magia Hogwarts e sventa il piano di Lord Voldemort per ottenere la Pietra Filosofale.");
-$movie3 = new Movie("Dune", "Eng", "Paul Atreides, erede di una potente famiglia, viaggia su Arrakis per estrarre una spezia preziosa. Affronta nemici e tradimenti per sopravvivere e realizzare il suo destino.");
-$movie4 = new Movie("American Beauty", "Eng", "Un uomo infelice riscopre la bellezza della vita attraverso l'amore di una giovane e l'amicizia di un vicino. Sfida le convenzioni sociali per raggiungere la vera felicità.");
+$movie1 = new Movie("Ritorno al futuro", "Eng", "Marty McFly viaggia indietro nel tempo, sconvolgendo accidentalmente la sua stessa esistenza. Deve riparare il passato per garantirsi il futuro.","https://pad.mymovies.it/filmclub/2006/06/011/locandinapg5.jpg", array("sci-fi", "azione"));
+$movie2 = new Movie("Harry Potter", "Ita", "Un orfano scopre di essere un mago, frequenta la scuola di magia Hogwarts e sventa il piano di Lord Voldemort per ottenere la Pietra Filosofale.", "https://static.posters.cz/image/1300/poster/harry-potter-la-pietra-filosofale-i104639.jpg", array("fantasy", "azione"));
+$movie3 = new Movie("Dune- Parte Uno", "Eng", "Paul Atreides, erede di una potente famiglia, viaggia su Arrakis per estrarre una spezia preziosa. Affronta nemici e tradimenti per sopravvivere e realizzare il suo destino.", "https://m.media-amazon.com/images/I/61aBECo7a2L._AC_UF1000,1000_QL80_.jpg", array("sci-fy", "azione"));
+$movie4 = new Movie("American Beauty", "Eng", "Un uomo infelice riscopre la bellezza della vita attraverso l'amore di una giovane e l'amicizia di un vicino. Sfida le convenzioni sociali per raggiungere la vera felicità.", "https://pad.mymovies.it/filmclub/2002/08/166/locandina288.jpg", array("introspetivo", "romantico"));
 
 $movies = [
     $movie1,
@@ -56,18 +63,27 @@ $movies = [
 
 <body>
     <div class="contianer-fluid">
-        <h1 class="display-1 text-success-emphasis text-center my-5 link-underline-secondary  ">Choose your Movie</h1>
-        <div class="container d-flex gap-2 justify-content-center">
+        <div class="container">
+            <h1 class="display-1 text-success p-4 rounded-4 text-center my-5 link-underline-secondary bg-opacity-75 bg-black fw-bold">Choose your Movie</h1>
+
+        </div>
+        <div class="container d-flex  justify-content-center gap-2  ">
 
             
                 <?php
                 foreach ($movies as $movie) {
                     echo "
-                        <div class='card w-100'>
-                            <div class='card-body'>
-                            <h5 class='card-title'>" . $movie->maker() . "</h5>
-                            <p class='card-text'>Lingua: " . $movie->language . "</p>
-                            <p>" . $movie->description . "</p>
+                        <div class='card border-0 mb-4 w-100' style= 'width= calc(100% / 2)'>
+                            <img src=" . $movie->playbill . " class=' ' style='height: 380px;'>
+
+                            <div class='card-body text-white bg-opacity-75 bg-black  rounded-bottom-3 '>
+                            <h5 class='card-title text-center fw-bold fs-4 '>" . $movie->maker() . "</h5>
+                            <p class='card-text text-center'>Lingua: " . $movie->language . "</p>
+                            <p >" . $movie->description . "</p>
+                            <p > Genere: " . $movie->type() . ".</p>
+
+                            
+
                             </div>
                         </div>";
                 }
